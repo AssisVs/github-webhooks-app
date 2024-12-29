b4c80a86bdd9e88b9964f68cc89854b9963fbda0
aja1010 <ajassis.vs@gmail.com>
2024-12-29 16:04:32 -0300

    primeiro commit

---
+++ TesteJs.cjs
@@ -0,0 +1,103 @@
+// You installed the `express` library earlier. For more information, see "[JavaScript example: Install dependencies](#javascript-example-install-dependencies)."
+const express = require('express');
+
+// This initializes a new Express application.
+const app = express();
+
+// This defines a POST route at the `/webhook` path. This path matches the path that you specified for the smee.io forwarding. For more information, see "[Forward webhooks](#forward-webhooks)."
+//
+// Once you deploy your code to a server and update your webhook URL, you should change this to match the path portion of the URL for your webhook.
+app.post('/github-webhooks', express.json({type: 'application/json'}), (request, response) => {
+
+  // Respond to indicate that the delivery was successfully received.
+  // Your server should respond with a 2XX response within 10 seconds of receiving a webhook delivery. If your server takes longer than that to respond, then GitHub terminates the connection and considers the delivery a failure.
+  response.status(202).send('Accepted');
+
+  // Check the `x-github-event` header to learn what event type was sent.
+  const githubEvent = request.headers['x-github-event'];
+
+  // You should add logic to handle each event type that your webhook is subscribed to.
+  // For example, this code handles the `issues` and `ping` events.
+  //
+  // If any events have an `action` field, you should also add logic to handle each action that you are interested in.
+  // For example, this code handles the `opened` and `closed` actions for the `issue` event.
+  //
+  // For more information about the data that you can expect for each event type, see "[AUTOTITLE](/webhooks/webhook-events-and-payloads)."
+  if (githubEvent === 'issues') {
+    const data = request.body;
+    const action = data.action;
+    if (action === 'opened') {
+      console.log(`An issue was opened with this title: ${data.issue.title}`);
+    } else if (action === 'closed') {
+      console.log(`An issue was closed by ${data.issue.user.login}`);
+    } else {
+      console.log(`Unhandled action for the issue event: ${action}`);
+    }
+  } else if (githubEvent === 'appId=cid-v1:') {
+    console.log('Insomnia sent the Signature event');
+  }
+  switch(githubEvent) {
+    case 'ping':
+        console.log(`An issue event: ${githubEvent}`);
+      break;
+    default:
+     console.log(`Unhandled event: ${githubEvent}`);
+   }
+});
+
+// This defines the port where your server should listen.
+// 3000 matches the port that you specified for webhook forwarding. For more information, see "[Forward webhooks](#forward-webhooks)."
+//
+// Once you deploy your code to a server, you should change this to match the port where your server is listening.
+const port = 8000;
+
+// This starts the server and tells it to listen at the specified port.
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+//copiada   a funçao verify signature
+let encoder = new TextEncoder();
+
+async function verifySignature(secret, header, payload) {
+    let parts = header.split("=");
+    let sigHex = parts[1];
+
+    let algorithm = { name: "HMAC", hash: { name: 'SHA-256' } };
+
+    let keyBytes = encoder.encode(secret);
+    let extractable = false;
+    let key = await crypto.subtle.importKey(
+        "raw",
+        keyBytes,
+        algorithm,
+        extractable,
+        [ "sign", "verify" ],
+    );
+
+    let sigBytes = hexToBytes(sigHex);
+    let dataBytes = encoder.encode(payload);
+    let equal = await crypto.subtle.verify(
+        algorithm.name,
+        key,
+        sigBytes,
+        dataBytes,
+    );
+
+    return equal;
+}
+
+function hexToBytes(hex) {
+    let len = hex.length / 2;
+    let bytes = new Uint8Array(len);
+
+    let index = 0;
+    for (let i = 0; i < hex.length; i += 2) {
+        let c = hex.slice(i, i + 2);
+        let b = parseInt(c, 16);
+        bytes[index] = b;
+        index += 1;
+    }
+
+    return bytes;
+}
---
+++ app/Jobs/PingWebhook.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Testing\Fluent\Concerns\Interaction;
+
+class PingWebhook implements ShouldQueue
+{
+    use Queueable;
+    use Dispatchable;
+    use InteractsWithQueue;
+    use SerializesModels;
+
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(public GitHubWebhookCall $webhookCall)
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        ray($this->webhookCall->eventActionName());
+    }
+}
